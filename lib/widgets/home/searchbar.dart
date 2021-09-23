91abc0486b8d5b860a6b9acf7e0430e200f96046
Keyul Jain <keyuljainkj@gmail.com>
2021-09-23 23:52:17 +0530

    SearchBar

--- lib/widgets/home/searchbar.dart
+++ lib/widgets/home/searchbar.dart
@@ -5,20 +5,22 @@ class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 330,
+      width: MediaQuery.of(context).size.width/1.1,
+      height: MediaQuery.of(context).size.height/16,
       child: TextField(
         decoration: new InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20, 5, 0, 5),
           fillColor: Colors.white,
           filled: true,
           suffixIcon: Icon(Icons.search),
           focusedBorder: OutlineInputBorder(
               borderSide:
               BorderSide(color: Colors.white, width: 5.0),
-              borderRadius: BorderRadius.circular(10)),
+              borderRadius: BorderRadius.circular(20)),
           enabledBorder: OutlineInputBorder(
             borderSide:
             BorderSide(color: Colors.black, width: 1.0),
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(15),
           ),
 
           hintText: 'Search',
