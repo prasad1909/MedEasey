58757a57d0ae7912716eeadd36231b60954b1393
PRITHVI <coolprithvi95@gmail.com>
2021-09-24 12:38:04 +0530

    :Sparkles: Success Screen

--- lib/screens/success_screen.dart
+++ lib/screens/success_screen.dart
@@ -10,18 +10,32 @@ class SuccessScreen extends StatelessWidget {
     return Scaffold(
       appBar: const BasicAppbar(),
       drawer: const AppDrawer(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text('Your Order has been placed Successfully'),
-          const Text('You can track your order details'),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/orders');
-              },
-              child: const Text('Your Orders'))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(55.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text('Your Order Has Been Placed',style: TextStyle(fontSize: 20), ),
+            const Text('Successfully',style: TextStyle(fontSize: 20),),
+            SizedBox(
+              width: 20,
+              height: 20,
+            ),
+            const Text('Track Your Order Details Below',style: TextStyle(fontSize: 15),),
+            Icon(Icons.arrow_downward_outlined),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.teal)
+              ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/orders');
+                },
+                child: const Text('Your Orders',style: TextStyle(fontSize: 16),),
+
+            )
+          ],
+        ),
       ),
     );
   }
