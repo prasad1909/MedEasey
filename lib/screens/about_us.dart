2c294a92901e17b4bf7774fa18dc0109766abc72
PRITHVI <coolprithvi95@gmail.com>
2021-10-10 21:30:44 +0530

    About Us Page

--- lib/screens/about_us.dart
+++ lib/screens/about_us.dart
@@ -22,7 +22,7 @@ class AboutUs extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(
-                  height: 20,
+                  height: 40,
                 ),
                 Container(
                   width: 310,
@@ -101,245 +101,9 @@ class AboutUs extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 20,
+                  height: 40,
                 ),
-                Container(
-                  width: 310,
-                  height: 125,
-                  decoration: BoxDecoration(
-                      color: Color(0xFF80FF9C),
-                      border: Border.all(
-                        width: 1,
-                        color: Colors.teal,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))
-                  ),
-                  child: Row(
-                    children: [ Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height:15),
-                        Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text("Keyul Jain",
-                              style: TextStyle(color: Colors.teal,
-                                  fontSize: 21, fontWeight: FontWeight.bold),
-                              textDirection: TextDirection.ltr,),
-                          ],
-                        ),
-                        SizedBox(height: 5,),
-                        Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text("Frontend Developer      ",
-                              style: TextStyle(
-                                color: Colors.teal.shade400,
-                                fontSize: 18.0,
-                              ),),
-                          ],
-                        ),
-                        SizedBox(height: 8,width: 8),
-                        Row(
-                          children: [
-                            SizedBox(width: 20,),
-                            CircleAvatar(
-                              backgroundColor: Colors.teal,
-                              radius: 12,
-                              child: Icon(
-                                Icons.email_rounded,
-                                color: Colors.white,
-                                size: 15,
-                              ),
-                            ),
-                            SizedBox(width: 10,),
-                            Text(
-                              'keyuljainkj@gmail.com',
-                              style: TextStyle(
-                                color: Colors.teal.shade700,
-                                fontSize: 12.0,
-                              ),
-                            ),
-                          ],
-                        ),],
-                    ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(width: 12,),
-                      Column(
-                        children: [
-                          SizedBox(height: 30,),
-                          CircleAvatar(
-                            backgroundColor: Colors.white,
-                            radius: 25,
-                              backgroundImage: AssetImage('assets/images/keyul.jpeg'),
-                          )
-                        ],
-                      ),],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 310,
-                  height: 125,
-                  decoration: BoxDecoration(
-                      color: Color(0xFF80FF9C),
-                      border: Border.all(
-                        width: 1,
-                        color: Colors.teal,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))
-                  ),
-                  child: Row(
-                    children: [ Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height:15),
-                        Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text("Pranav Dani",
-                              style: TextStyle(color: Colors.teal,
-                                  fontSize: 21, fontWeight: FontWeight.bold),
-                              textDirection: TextDirection.ltr,),
-                          ],
-                        ),
-                        SizedBox(height: 5,),
-                        Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text("Graphic Designer           ",
-                              style: TextStyle(
-                                color: Colors.teal.shade400,
-                                fontSize: 18.0,
-                              ),),
-                          ],
-                        ),
-                        SizedBox(height: 8,width: 8),
-                        Row(
-                          children: [
-                            SizedBox(width: 20,),
-                            CircleAvatar(
-                              backgroundColor: Colors.teal,
-                              radius: 12,
-                              child: Icon(
-                                Icons.email_rounded,
-                                color: Colors.white,
-                                size: 15,
-                              ),
-                            ),
-                            SizedBox(width: 10,),
-                            Text(
-                              'pranavpdani@gmail.com',
-                              style: TextStyle(
-                                color: Colors.teal.shade700,
-                                fontSize: 12.0,
-                              ),
-                            ),
-                          ],
-                        ),],
-                    ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(width: 12,),
-                      Column(
-                        children: [
-                          SizedBox(height: 30,),
-                          CircleAvatar(
-                            backgroundColor: Colors.white,
-                            radius: 25,
-                            backgroundImage: AssetImage('assets/images/pranav.png'),
-                          )
-                        ],
-                      ),],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  width: 310,
-                  height: 125,
-                  decoration: BoxDecoration(
-                      color: Color(0xFF80FF9C),
-                      border: Border.all(
-                        width: 1,
-                        color: Colors.teal,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0))
-                  ),
-                  child: Row(
-                    children: [ Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(height:15),
-                        Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text("Hardik Raheja",
-                              style: TextStyle(color: Colors.teal,
-                                  fontSize: 21, fontWeight: FontWeight.bold),
-                              textDirection: TextDirection.ltr,),
-                          ],
-                        ),
-                        SizedBox(height: 5,),
-                        Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text("Backend Developer       ",
-                              style: TextStyle(
-                                color: Colors.teal.shade400,
-                                fontSize: 18.0,
-                              ),),
-                          ],
-                        ),
-                        SizedBox(height: 8,width: 8),
-                        Row(
-                          children: [
-                            SizedBox(width: 20,),
-                            CircleAvatar(
-                              backgroundColor: Colors.teal,
-                              radius: 12,
-                              child: Icon(
-                                Icons.email_rounded,
-                                color: Colors.white,
-                                size: 15,
-                              ),
-                            ),
-                            SizedBox(width: 10,),
-                            Text(
-                              'raheja.hardik@gmail.com',
-                              style: TextStyle(
-                                color: Colors.teal.shade700,
-                                fontSize: 12.0,
-                              ),
-                            ),
-                          ],
-                        ),],
-                    ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(width: 12,),
-                      Column(
-                        children: [
-                          SizedBox(height: 30,),
-                          CircleAvatar(
-                            backgroundColor: Colors.white,
-                            radius: 25,
-                            backgroundImage: AssetImage('assets/images/hardik.png'),
-                          )
-                        ],
-                      ),],
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
+
                 Container(
                   width: 310,
                   height: 125,
@@ -417,7 +181,84 @@ class AboutUs extends StatelessWidget {
                   ),
                 ),
                 SizedBox(
-                  height: 20,
+                  height: 40,
+                ),
+
+                Container(
+                  width: 310,
+                  height: 125,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF80FF9C),
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.teal,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))
+                  ),
+                  child: Row(
+                    children: [ Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height:15),
+                        Row(
+                          children: [
+                            SizedBox(width: 20),
+                            Text("Aanchal Jogeshwar",
+                              style: TextStyle(color: Colors.teal,
+                                  fontSize: 21, fontWeight: FontWeight.bold),
+                              textDirection: TextDirection.ltr,),
+                          ],
+                        ),
+                        SizedBox(height: 5,),
+                        Row(
+                          children: [
+                            SizedBox(width: 20),
+                            Text("Design",
+                              style: TextStyle(
+                                color: Colors.teal.shade400,
+                                fontSize: 18.0,
+                              ),),
+                          ],
+                        ),
+                        SizedBox(height: 8,width: 8),
+                        Row(
+                          children: [
+                            SizedBox(width: 20,),
+                            CircleAvatar(
+                              backgroundColor: Colors.teal,
+                              radius: 12,
+                              child: Icon(
+                                Icons.email_rounded,
+                                color: Colors.white,
+                                size: 15,
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Text(
+                              'aanchal123@gmail.com',
+                              style: TextStyle(
+                                color: Colors.teal.shade700,
+                                fontSize: 12.0,
+                              ),
+                            ),
+                          ],
+                        ),],
+                    ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(width: 12,),
+                      Column(
+                        children: [
+                          SizedBox(height: 30,),
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 25,
+                            backgroundImage: AssetImage('assets/images/image.png'),
+                          )
+                        ],
+                      ),],
+                  ),
                 ),
               ],
             ),
